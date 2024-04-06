d9f769bf9421a4d9be16e99606d727141e5abc8f
Tom Barten <17269387+TomBarten@users.noreply.github.com>
2024-04-06 23:45:26 +0200

    Cleanup files after rename

--- src/OpenTelemetry.Demo/NumberService/DiagnosticNames.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NumberService;
-
-internal static class DiagnosticNames
-{
-    public const string AmountOfNumbers = "numbers.amount";
-    public const string AmountIsLessThanOne = "numbers.amount.is_less_than_one";
-}
